0943e7aa8d432af1a6ca9900f35cfb658617484a
jinggumii <jinggumi9@gmail.com>
2021-06-12 17:53:05 +0900

    complete

--- mysite/titanic/models/service.py
+++ mysite/titanic/models/service.py
@@ -1,6 +1,9 @@
 from mysite.titanic.models.dataset import Dataset
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
 
 class Service(object):
 
@@ -8,6 +11,92 @@ class Service(object):
 
     def new_model(self, payload) -> object:
         this = self.dataset
-        this.context = '../data/'
+        this.context = './data/'
         this.fname = payload
         return pd.read_csv(this.context + this.fname)
+
+    @staticmethod
+    def create_train(this) -> object:
+        return this.train.drop('Survived', axis=1) # axis 0 행(가로) 1 열(세로)
+
+    @staticmethod
+    def create_label(this) -> object:
+        return this.train['Survived']
+
+    @staticmethod
+    def drop_feature(this, *feature) -> object:
+
+        for i in feature:
+            this.train = this.train.drop([i], axis=1)
+            this.test = this.test.drop([i], axis=1)
+            # 학습, 테스트 세트는 항상 동일하게 편집한다.
+        return this
+
+    @staticmethod
+    def embarked_norminal(this) -> object: # this 가 변형될거니까 static ?
+        this.train = this.train.fillna({'Embarked': 'S'}) # S는 사우스햄튼
+        this.test = this.test.fillna({'Embarked': 'S'})  # S는 사우스햄튼
+        this.train['Embarked'] = this.train['Embarked'].map({'S': 1, 'C': 2, 'Q': 3})
+        this.test['Embarked'] = this.test['Embarked'].map({'S': 1, 'C': 2, 'Q': 3})
+        return this
+
+    @staticmethod
+    def title_norminal(this) -> object:
+        combine = [this.train, this.test]
+        for dataset in combine:
+            dataset['Title'] = dataset.Name.str.extract('([A-Za-z]+)\.', expand=False)
+        for dataset in combine:
+            dataset['Title'] = dataset['Title'].replace(
+                ['Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Jonkheer', 'Dona'], 'Rare')
+            dataset['Title'] = dataset['Title'].replace(['Countess', 'Lady', 'Sir'], 'Royal')
+            dataset['Title'] = dataset['Title'].replace('Mlle', 'Mr')
+            dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+            dataset['Title'] = dataset['Title'].replace('Mme', 'Rare')
+            title_mapping = {'Mr': 1, 'Miss': 2, 'Mrs': 3, 'Master': 4, 'Royal': 5, 'Rare': 6}
+            dataset['Title'] = dataset['Title'].fillna(0)
+            dataset['Title'] = dataset['Title'].map(title_mapping)
+        return this
+
+    @staticmethod
+    def gender_norminal(this) -> object:
+        combine = [this.train, this.test]
+        gender_mapping = {'male': 1, 'female': 2}
+        for i in combine:
+            i['Gender'] = i['Sex'].map(gender_mapping)
+        return this
+
+    @staticmethod
+    def age_ordinal(this) -> object:
+        train = this.train
+        test = this.test
+        for data in train, test:
+            data['Age'] = data['Age'].fillna(-0.5)
+        bins = [-1, 0, 5, 12, 18, 24, 35, 68, np.inf]
+        labels = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']
+        age_title_mapping = {'Unknown': 0, 'Baby': 1, 'Child': 2, 'Teenager': 3, 'Student': 4,
+                             'Young Adult': 5, 'Adult': 6, 'Senior': 7}
+        for data in train, test:
+            data['AgeGroup'] = pd.cut(data['Age'], bins=bins, labels=labels)
+            data['AgeGroup'] = data['AgeGroup'].map(age_title_mapping)
+        return this
+
+    @staticmethod
+    def fare_ordinal(this) -> object:
+
+        this.train['FareBand'] = pd.qcut(this.train['Fare'], 4, labels={1,2,3,4}) # 최고의 최저를 통해 4등분하라
+        this.test['FareBand'] = pd.qcut(this.test['Fare'], 4, labels={1,2,3,4}) # 최고의 최저를 통해 4등분하라
+        return this
+
+    @staticmethod
+    def create_k_fold() -> object:
+        return KFold(n_splits=10, shuffle= True, random_state=0) # 트레인데이터를 10등분, 반복출제 허용
+
+    def get_accuracy(self, this):
+        score = cross_val_score(RandomForestClassifier(),
+                                this.train,
+                                this.label,
+                                cv=self.create_k_fold(),
+                                n_jobs=1,
+                                scoring='accuracy')
+        return round(np.mean(score)*100, 2)
+
--- mysite/titanic/views/controller.py
+++ mysite/titanic/views/controller.py
@@ -1,18 +1,60 @@
 from mysite.titanic.models.dataset import Dataset
 from mysite.titanic.models.service import Service
 from mysite.titanic.templates.plot import Plot
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 class Controller(object):
 
     dataset = Dataset()
     service = Service()
 
-if __name__ == '__main__':
-    api = Controller()
-    while 1:
-        menu = input('0-종료  1-데이터출력')
-        if menu == '0':
-            break
-        elif menu == '1':
-            plot = Plot('train.csv')
-            plot.print_servived_dead()
+    def modeling(self, train, test) -> object:
+        service = self.service
+        this = self.preprocess(train, test)
+        this.label = service.create_label(this)
+        this.train = service.create_train(this)
+        return this
+
+    def learning(self, train, test):
+        this = self.modeling(train, test)
+        print(f'사이킷런의 SVC 알고리즘 정확도 {self.service.get_accuracy(this)} %')
+
+    def submit(self, train, test):
+        this = self.modeling(train, test)
+        clf = RandomForestClassifier()
+        clf.fit(this.train, this.label)
+        prediction = clf.predict(this.test)
+        pd.DataFrame({'PassengerId': this.id, 'Survived': prediction}).to_csv('./data/submission.csv', index=False)
+
+    def preprocess(self, train, test) -> object:
+        service = self.service
+        this = self.dataset
+        this.train = service.new_model(train)
+        this.test = service.new_model(test)
+        this.id = this.test['PassengerId']
+        this = service.embarked_norminal(this)
+        this = service.title_norminal(this)
+        this = service.gender_norminal(this)
+        this = service.age_ordinal(this)
+        this = service.fare_ordinal(this)
+        this = service.drop_feature(this, 'Name', 'Sex', 'Cabin', 'Ticket', 'Age', 'Fare')
+
+        self.print_this(this)
+        return this
+
+    @staticmethod
+    def print_this(this):
+        print('*' * 100)
+        print(f'1. Train 의 type \n {type(this.train)} ')
+        print(f'2. Train 의 column \n {this.train.columns} ')
+        print(f'3. Train 의 상위 1개 행\n {this.train.head()} ')
+        print(f'4. Train 의 null 의 갯수\n {this.train.isnull().sum()}개')
+        print(f'5. Test 의 type \n {type(this.test)}')
+        print(f'6. Test 의 column \n {this.test.columns}')
+        print(f'7. Test 의 상위 1개 행\n {this.test.head()}개')
+        print(f'8. Test 의 null 의 갯수\n {this.test.isnull().sum()}개')
+        print('*' * 100)
+
+
